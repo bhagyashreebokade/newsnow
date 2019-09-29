bfb388a0a1c6da8dfe0bc04db781da7151b68dfc
bhagyashreebokade <44973829+bhagyashreebokade@users.noreply.github.com>
2019-09-29 22:59:19 +0530

    initial commit

---
+++ newsApi.js
@@ -0,0 +1,4 @@
+const apiKey = '7f65a98cdbf1496088f8cb61e23fd2e8';
+const topHeadlinesUrl = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=45b42b4a800c4b6cb7da2ce682ad6866';
+
+export { apiKey, topHeadlinesUrl };
